86d83f444c29e64d6afd76c268681a539df4b685
annder <2461067798@qq.com>
2018-05-21 22:51:40 +0800

    a

--- Test_C_On_Pointer/string.cpp
+++ Test_C_On_Pointer/string.cpp
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 char str_chr(char *string,char char_find) {
 	char *temp = string;
 	while (*string++ == char_find)
@@ -37,3 +39,29 @@ const char *my_strrstr(char const *stringA,char const *stringB ) {
 };
 
 
+void count_persent_string(char *string) {
+	char *val,*temp;
+	scanf("%s", &val);
+	strcpy(val,temp);
+	int upper_case = 0,
+		lowwer_case = 0,
+		whithe_case = 0,
+		symbol_case = 0,
+		number_case = 0;
+	while (*val++ != '\0' ) {
+		int force_transform = (int)val;
+		if (isupper(force_transform)) 
+			upper_case++;
+		if (islower(force_transform))
+			lowwer_case++;
+		if (isalnum(force_transform))
+			number_case++;
+		if (isblank(force_transform))
+			whithe_case++;
+	};
+	int sumlen = strlen(temp);
+	printf("blank: %f \n",  whithe_case/ sumlen);
+	printf("number: %f \n", number_case / sumlen);
+	printf("upper:%f \n", upper_case / sumlen);
+	printf("lowwer:%f \n", lowwer_case / sumlen);
+}
